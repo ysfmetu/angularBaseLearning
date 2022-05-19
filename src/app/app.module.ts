861d9a75fe4e980f287efeeebfbf04c18796d165
ysfmetu <ysf_metu@hotmail.com>
2022-05-19 14:56:01 +0300

    Angular data-binding(one-way data-binding) ve temel html tag kullanımları 1_ts kısmında değer verdiğimiz property,html kısmında çağırıp değerini okumak//propery event 2_Event kullanımını görüyoruz//buton clik özelliği ile fonksiyon içindeki mesajı okuyoruz//property event 3_text içerisindeki değeri alert ile ekrana veriyoruz//event binding example 4_two-way databinding ilede bir örnek çalışma  yapılmıştır.ts kısmında gelen değerler html kısmında okunmuştur.. 5_Component oluşturuldu ve main component içerisinde çağırıldı 6_ikinci bir component tanımlanıp birbiri içerisinde çağırıldı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 import {TooltipModule} from "ngx-bootstrap/tooltip";
-import {ModalModule} from "ngx-bootstrap/modal";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule} from "@angular/forms";
+import { ProductComponent } from './components/product/product.component';
+import { SellerComponent } from './components/seller/seller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    SellerComponent
   ],
     imports: [
         BrowserModule,
         BsDropdownModule.forRoot(),
         TooltipModule.forRoot(),
-        ModalModule.forRoot(),
         FormsModule
     ],
   providers: [],
